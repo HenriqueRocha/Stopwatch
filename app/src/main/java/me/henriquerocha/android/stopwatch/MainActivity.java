84edaa0a74bda7f1a9de6295be38a14a30f58fb7
Henrique Rocha <hmrocha@gmail.com>
2019-11-11 14:07:09 +0100

    Change start button to start/stop.

--- app/src/main/java/me/henriquerocha/android/stopwatch/MainActivity.java
+++ app/src/main/java/me/henriquerocha/android/stopwatch/MainActivity.java
@@ -10,29 +10,41 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    private Handler handler = new Handler();
+    private final Handler handler = new Handler();
+    private final Runnable stopwatchRunnable = new StopwatchRunnable();
+
+    final long initialTime = SystemClock.elapsedRealtime();
+
+    private boolean isRunning;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = findViewById(R.id.startButton);
+        final Button startButton = findViewById(R.id.startStopButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final long initialTime = SystemClock.elapsedRealtime();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView stopwatchTextView = findViewById(R.id.stopwatchTextView);
-                        long elapsedRealtime = SystemClock.elapsedRealtime();
-                        stopwatchTextView.setText(String.valueOf(elapsedRealtime - initialTime));
-                        handler.post(this);
-                    }
-                });
-
+                if (isRunning) {
+                   startButton.setText(R.string.start);
+                   handler.removeCallbacks(stopwatchRunnable);
+                } else {
+                    startButton.setText(R.string.stop);
+                    handler.post(stopwatchRunnable);
+                }
+                isRunning = !isRunning;
             }
         });
+    }
+
+    private class StopwatchRunnable implements Runnable {
+        @Override
+        public void run() {
+            TextView stopwatchTextView = findViewById(R.id.stopwatchTextView);
+            long elapsedRealtime = SystemClock.elapsedRealtime();
+            stopwatchTextView.setText(String.valueOf(elapsedRealtime - initialTime));
+            handler.post(this);
+        }
     }
 }
